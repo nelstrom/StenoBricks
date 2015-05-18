79a1a325659f7ba1759673827b27e05fbced8003
Drew Neil <andrew.jr.neil@gmail.com>
2015-05-18 17:45:55 +0100

    Annotate points

--- public/scratch.js
+++ public/scratch.js
@@ -1,11 +1,11 @@
 var draw = SVG('drawing').size(300, 300);
 
 var points = [
-  'M100,100',
-  'L200,100',
-  'L200,200',
-  'L100,200',
-  'z'
+  'M100,100',  // top left
+  'L200,100',  // top right
+  'L200,200',  // bottom right
+  'L100,200',  // bottom left
+  'z'          // close path (back to top left)
 ];
 
 var shape = draw.path(points.join(','))
