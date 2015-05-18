aa0e5357e56dddfd6caaeb5cfd3d6200e7775adb
Drew Neil <andrew.jr.neil@gmail.com>
2015-05-18 18:10:52 +0100

    Create a topRowPoints path

--- public/scratch.js
+++ public/scratch.js
@@ -3,6 +3,24 @@ var darkfill    = '#555';
 var darkstroke  = '#222';
 var draw = SVG('drawing').size(500, 300);
 
+var topRowPoints = [
+  'M 100, 100',  // top left
+
+  // top bump
+  'C 100, 080',  // control point 1
+  '  150, 080',  // control point 2
+  '  150, 100',  // top right
+
+  // top edge
+  'L 200, 100',  // top middle
+
+  'L 200, 150',  // bottom right
+  'C 200, 210',
+  '  100, 210',
+  '  100, 150',  // bottom left
+  'z'            // close path (back to top left)
+];
+
 var bottomRowPoints = [
   'M 100, 100',  // top left
 
@@ -25,7 +43,7 @@ var shape = draw.path(bottomRowPoints.join(','))
   .stroke({ color: darkstroke, width: 2 })
   .fill(darkfill);
 
-var shape = draw.path(bottomRowPoints.join(','))
+var shape = draw.path(topRowPoints.join(','))
   .transform({
     x:100,
     y:0
