c908719e79090d8fac0a18aac9fa4f693de58d20
Drew Neil <andrew.jr.neil@gmail.com>
2015-05-18 17:49:27 +0100

    Add a 'top bump' to the path

--- public/scratch.js
+++ public/scratch.js
@@ -1,10 +1,15 @@
 var draw = SVG('drawing').size(300, 300);
 
 var points = [
-  'M100,100',  // top left
-  'L200,100',  // top right
-  'L200,200',  // bottom right
-  'L100,200',  // bottom left
+  'M 100, 100',  // top left
+  // top bump
+  'L 150, 100',
+  'L 155, 090',
+  'L 175, 088',
+  'L 195, 090',
+  'L 200, 100',  // top right
+  'L 200, 200',  // bottom right
+  'L 100, 200',  // bottom left
   'z'          // close path (back to top left)
 ];
 
