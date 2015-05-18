a538966462ba697283f7b413f11a2fb8199eb8db
Drew Neil <andrew.jr.neil@gmail.com>
2015-05-18 17:30:39 +0100

    Add a circle and use it to mask the rectangle

--- public/scratch.js
+++ public/scratch.js
@@ -1,2 +1,14 @@
 var draw = SVG('drawing').size(300, 300);
-var rect = draw.rect(100, 100).attr({ fill: '#ffccff' });
+
+var rect = draw.rect(100, 100);
+rect.fill({ color: '#eee' });
+// rect.stroke({ color: '#333', width: 3 });
+
+var circle = draw.circle(100);
+circle.fill('#ccc');
+circle.transform({
+  x: 0,
+  y: 50
+});
+
+rect.maskWith(circle);
