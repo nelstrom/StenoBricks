e61d941513f57212386d4d894c8832b0a3709712
Drew Neil <andrew.jr.neil@gmail.com>
2015-05-24 18:29:35 +0100

    Bake the knowledge of key sizes into spanKeys

--- source/javascripts/scratch.js
+++ source/javascripts/scratch.js
@@ -391,11 +391,37 @@ function drawMatcherPoints(positionList) {
 // drawMatcherPoints([10]);
 // drawMatcherPoints([12,13,14,15,16,17,18,19,20,21,22,23]);
 
+var keyDimensions = {
+   0: { left:  0, right:  2 },
+   1: { left:  2, right:  3 },
+   2: { left:  3, right:  4 },
+   3: { left:  4, right:  5 },
+   4: { left:  5, right:  6 },
+   5: { left:  6, right:  7 },
+   6: { left:  7, right:  8 },
+   7: { left:  8, right:  9 },
+   8: { left:  9, right: 10 },
+   9: { left: 10, right: 11 },
+  10: { left: 11, right: 13 },
+  11: { left: 13, right: 14 },
+  12: { left: 14, right: 15 },
+  13: { left: 15, right: 16 },
+  14: { left: 16, right: 17 },
+  15: { left: 17, right: 18 },
+  16: { left: 18, right: 19 },
+  17: { left: 19, right: 20 },
+  18: { left: 20, right: 21 },
+  19: { left: 21, right: 22 },
+  20: { left: 22, right: 23 },
+  21: { left: 23, right: 24 },
+  22: { left: 24, right: 25 },
+};
+
 function spanKeys(keyList) {
-  var first = keyList[0];
-  var last  = keyList[keyList.length-1];
-  var width = last - first + 1;
-  var xOffset = first * 50;
+  var first = keyDimensions[keyList[0]];
+  var last  = keyDimensions[keyList[keyList.length-1]];
+  var width = last.right - first.left;
+  var xOffset = first.left * 50 - 50;
   var rightEdge = 95 + (width * 50);
   var shapePath = [
     'M 105, 100',  // top left
