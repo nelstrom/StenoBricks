355236bcfb312da704ebdcd9f03e01841aa0ba5f
Drew Neil <andrew.jr.neil@gmail.com>
2015-06-06 22:27:37 +0100

    Use "-" in middle of stroke if no vowels (or star)

--- lib/brick_mapper.rb
+++ lib/brick_mapper.rb
@@ -1,4 +1,7 @@
 class BrickMapper
+  START  = (0..7).to_a
+  MIDDLE = (8..12).to_a
+  FINISH = (13..23).to_a
   KEY_LABELS = %w(# s t k p w h r a o * e u f r p b l g t s d z)
   #               0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2
   #                                  10                  20
@@ -12,10 +15,25 @@ class BrickMapper
   end
 
   def lookup(definition)
-    definition.fetch(:bricks).map { |brick|
+    keyNumbers = definition.fetch(:bricks).map { |brick|
       brickset[brick]
-    }.flatten.map { |keystroke|
+    }.flatten
+
+    startKeys = keyNumbers.select { |i| START.include?(i) }
+    middleKeys = keyNumbers.select { |i| MIDDLE.include?(i) }
+    finishKeys = keyNumbers.select { |i| FINISH.include?(i) }
+
+    startLabels = startKeys.map { |keystroke|
       KEY_LABELS[keystroke]
-    }.join
+    }
+    middleLabels = middleKeys.map { |keystroke|
+      KEY_LABELS[keystroke]
+    }
+    finishLabels = finishKeys.map { |keystroke|
+      KEY_LABELS[keystroke]
+    }
+    middleLabels = '-' if middleLabels.length == 0
+    [startLabels, middleLabels, finishLabels].join
   end
+
 end
