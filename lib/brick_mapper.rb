7d5651e27d9947be6e371a90a3cfd640369500d6
Drew Neil <andrew.jr.neil@gmail.com>
2015-06-06 21:40:14 +0100

    Use each_with_object({}) for better readability
    http://stackoverflow.com/a/15761466/128850

--- lib/brick_mapper.rb
+++ lib/brick_mapper.rb
@@ -6,7 +6,9 @@ class BrickMapper
   attr_reader :brickset
 
   def initialize(bricks)
-    @brickset = Hash[bricks.map { |b| [b[:id], b[:keystrokes]] } ]
+    @brickset = bricks.each_with_object({}) do |brick, hash|
+      hash[brick[:id]] = brick[:keystrokes]
+    end
   end
 
   def lookup(definition)
