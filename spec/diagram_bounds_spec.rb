ee6fc9258fa8d6adc063014c655fed7ccbd43979
Drew Neil <andrew.jr.neil@gmail.com>
2015-12-02 16:39:38 +0000

    Refactor diagram_bounds_spec

--- spec/diagram_bounds_spec.rb
+++ spec/diagram_bounds_spec.rb
@@ -2,8 +2,13 @@ require 'rspec'
 require_relative '../lib/diagram_bounds'
 
 describe DiagramBounds do
-  it '#viewBox has default values' do
-    expect(DiagramBounds.new.view_box).to eql('0 0 1350 500')
+
+  context 'default bounds' do
+    subject(:default) { DiagramBounds.new }
+    it '#viewBox is "0 0 1350 500"' do
+      expect(default.view_box).to eql('0 0 1350 500')
+    end
   end
+
 end
 
