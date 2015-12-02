04cd18b1a0099c90526f9c1eeded1767178e4644
Drew Neil <andrew.jr.neil@gmail.com>
2015-12-02 17:38:27 +0000

    Refactor DiagramBounds specs

--- spec/diagram_bounds_spec.rb
+++ spec/diagram_bounds_spec.rb
@@ -4,7 +4,6 @@ require_relative '../lib/diagram_bounds'
 describe DiagramBounds do
 
   let(:monostroke_definition) { double("Mono", chords: Array.new(1)) }
-  let(:twostroke_definition)  { double("Mono", chords: Array.new(2)) }
   let(:fivestroke_definition) { double("Mono", chords: Array.new(5)) }
 
   context 'default bounds' do
@@ -34,22 +33,6 @@ describe DiagramBounds do
 
     it '#offset_for_chord is 0' do
       expect(monostroke_bounds.offset_for_chord).to eql(0)
-    end
-  end
-
-  context 'with a two-stroke definition' do
-    subject(:twostroke_bounds) { DiagramBounds.new(twostroke_definition) }
-    it '#viewBox is "0 0 1350 600"' do
-      expect(twostroke_bounds.view_box).to eql('0 0 1350 600')
-    end
-
-    it '#extra_height is 100' do
-      expect(twostroke_bounds.extra_height).to eql(100)
-    end
-
-    it '#offset_for_chord is 0' do
-      expect(twostroke_bounds.offset_for_chord(0)).to eql(-100)
-      expect(twostroke_bounds.offset_for_chord(1)).to eql(0)
     end
   end
 
