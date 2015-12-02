b91011bab554f00867a38cd95c37446981083523
Drew Neil <andrew.jr.neil@gmail.com>
2015-12-02 16:56:14 +0000

    Customize DiagramBounds#view_box height for multi-stroke case

--- lib/diagram_bounds.rb
+++ lib/diagram_bounds.rb
@@ -1,5 +1,24 @@
 class DiagramBounds
+  BASE_WIDTH  = 1350
+  BASE_HEIGHT = 500
+  CHORD_HEIGHT = 100
+
+  def initialize(definition=nil)
+    @chord_count = 1
+    if definition
+      @chord_count = definition.chords.count
+    end
+  end
+
   def view_box
-    '0 0 1350 500'
+    [0, 0, width, height].join(' ')
+  end
+
+  def width
+    BASE_WIDTH
+  end
+
+  def height
+    BASE_HEIGHT + CHORD_HEIGHT * (@chord_count-1)
   end
 end
--- spec/diagram_bounds_spec.rb
+++ spec/diagram_bounds_spec.rb
@@ -3,6 +3,10 @@ require_relative '../lib/diagram_bounds'
 
 describe DiagramBounds do
 
+  let(:monostroke_definition) { double("Mono", chords: Array.new(1)) }
+  let(:twostroke_definition)  { double("Mono", chords: Array.new(2)) }
+  let(:fivestroke_definition) { double("Mono", chords: Array.new(5)) }
+
   context 'default bounds' do
     subject(:default) { DiagramBounds.new }
     it '#viewBox is "0 0 1350 500"' do
@@ -10,5 +14,25 @@ describe DiagramBounds do
     end
   end
 
-end
+  context 'with a mono-stroke definition' do
+    subject(:monostroke_bounds) { DiagramBounds.new(monostroke_definition) }
+    it '#viewBox is "0 0 1350 500"' do
+      expect(monostroke_bounds.view_box).to eql('0 0 1350 500')
+    end
+  end
 
+  context 'with a two-stroke definition' do
+    subject(:twostroke_bounds) { DiagramBounds.new(twostroke_definition) }
+    it '#viewBox is "0 0 1350 600"' do
+      expect(twostroke_bounds.view_box).to eql('0 0 1350 600')
+    end
+  end
+
+  context 'with a five-stroke definition' do
+    subject(:fivestroke_bounds) { DiagramBounds.new(fivestroke_definition) }
+    it '#viewBox is "0 0 1350 900"' do
+      expect(fivestroke_bounds.view_box).to eql('0 0 1350 900')
+    end
+  end
+
+end
