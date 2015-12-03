422d281a5399ef90b07b2209490e99d07a497822
Drew Neil <andrew.jr.neil@gmail.com>
2015-12-03 15:29:08 +0000

    wip - update the definition_exploder_spec

--- lib/definition_exploder.rb
+++ lib/definition_exploder.rb
@@ -1,3 +1,13 @@
 class DefinitionExploder
 
 end
+
+class DefinitionExploder
+  attr_reader :brickset
+
+  def initialize(bricks)
+    @brickset = bricks.each_with_object({}) do |brick, hash|
+      hash[brick[:id]] = brick[:keystrokes]
+    end
+  end
+end
--- spec/definition_exploder_spec.rb
+++ spec/definition_exploder_spec.rb
@@ -2,8 +2,6 @@ require 'rspec'
 require_relative '../lib/definition_exploder'
 
 describe DefinitionExploder do
-  # "SAF": "save",
-  # "SAEUF": "safe",
   let(:bricks) do
     [
       {
@@ -34,14 +32,76 @@ describe DefinitionExploder do
     ]
   end
 
-  let(:save_chord) { }
+  let(:save_notation) { 'SAF' }
+  let(:safe_notation) { 'SAEUF' }
 
-  # xit 'works like this' do
+  it 'works like this' do
+    exploder = DefinitionExploder.new(bricks)
+    possibilities = exploder.explode(save_notation)
+    expect(possibilities).to eql([
+      ["start-s", "short-a", "end-f"],
+      ["start-s", "short-a", "end-v"],
+    ])
+  end
+end
+
+describe DefinitionExploder do
+  let(:bricks) do
+    [
+      {
+        "id": "start-s",
+        "label": "S",
+        "keystrokes": [1]
+      },
+      {
+        "id": "short-a",
+        "label": "A",
+        "keystrokes": [8]
+      },
+      {
+        "id": "long-a",
+        "label": "AY",
+        "keystrokes": [8,11,12]
+      },
+      {
+        "id": "short-e",
+        "label": "E",
+        "keystrokes": [11]
+      },
+      {
+        "id": "short-u",
+        "label": "U",
+        "keystrokes": [12]
+      },
+      {
+        "id": "short-i",
+        "label": "I",
+        "keystrokes": [11,12]
+      },
+      {
+        "id": "end-f",
+        "label": "F",
+        "keystrokes": [13]
+      },
+      {
+        "id": "end-v",
+        "label": "V",
+        "keystrokes": [13]
+      }
+    ]
+  end
+
+  let(:i_notation) { 'EU' }
+  let(:save_notation) { 'SAF' }
+  let(:safe_notation) { 'SAEUF' }
+
+  # it 'works like this' do
   #   exploder = DefinitionExploder.new(bricks)
-  #   possibilities = exploder.explode(save_chord)
+  #   possibilities = exploder.explode(i_notation).map{ |p| p["id"] }
   #   expect(possibilities).to eql([
-  #     ["start-s", "short-a", "end-f"],
-  #     ["start-s", "short-a", "end-v"],
+  #     ["short-i"],
+  #     ["short-e", "short-u"],
   #   ])
   # end
+
 end
