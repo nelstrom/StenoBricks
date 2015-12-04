4619c7b1b37bd30e962083a1a52e351c700759d5
Drew Neil <andrew.jr.neil@gmail.com>
2015-12-04 14:34:21 +0000

    Fix broken specs

--- spec/steno_brick_kit_spec.rb
+++ spec/steno_brick_kit_spec.rb
@@ -14,45 +14,45 @@ module StenoBrickKit
       button = StenoBrickKit.starButton.first
       expect(button[:shade]).to eql('light')
       expect(button[:symbol]).to eql('#starKeyPath')
-      expect(button[:offset]).to eql(4.5)
+      expect(button[:offset]).to eql(5)
     end
 
     it 'thumbs' do
       first, _, _, last = StenoBrickKit.thumbs
       expect(first[:shade]).to eql('dark')
       expect(first[:symbol]).to eql('#thumbFirstPath')
-      expect(first[:offset]).to eql(3.5)
+      expect(first[:offset]).to eql(4)
       expect(last[:shade]).to eql('light')
       expect(last[:symbol]).to eql('#thumbSecondPath')
-      expect(last[:offset]).to eql(6.5)
+      expect(last[:offset]).to eql(7)
     end
 
     it 'leftBottom' do
       button = StenoBrickKit.leftBottom.first
       expect(button[:shade]).to eql('light')
       expect(button[:symbol]).to eql('#bottomRowPath')
-      expect(button[:offset]).to eql(0)
+      expect(button[:offset]).to eql(0.5)
     end
 
     it 'leftTop' do
       button = StenoBrickKit.leftTop.first
       expect(button[:shade]).to eql('dark')
       expect(button[:symbol]).to eql('#topRowPath')
-      expect(button[:offset]).to eql(1)
+      expect(button[:offset]).to eql(1.5)
     end
 
     it 'rightBottom' do
       button = StenoBrickKit.rightBottom.first
       expect(button[:shade]).to eql('light')
       expect(button[:symbol]).to eql('#bottomRowPath')
-      expect(button[:offset]).to eql(7)
+      expect(button[:offset]).to eql(7.5)
     end
 
     it 'rightTop' do
       button = StenoBrickKit.rightTop.first
       expect(button[:shade]).to eql('dark')
       expect(button[:symbol]).to eql('#topRowPath')
-      expect(button[:offset]).to eql(7)
+      expect(button[:offset]).to eql(7.5)
     end
 
   end
--- spec/steno_spec.rb
+++ spec/steno_spec.rb
@@ -18,11 +18,11 @@ module Steno
       context 'with one keystroke, 2 units wide' do
         subject { star }
         it 'spans a range covering two units' do
-          expect(subject.span).to eql({start: 11, width: 2})
+          expect(subject.span).to eql({start: 12, width: 2})
         end
         it 'returns matchers labelled dark/light' do
           expect(subject.matchers).to eql([
-            {start: 11, width: 2, shade: 'light'}
+            {start: 12, width: 2, shade: 'light'}
           ])
         end
         it 'returns the text label' do
@@ -30,7 +30,7 @@ module Steno
         end
 
         it '#midpoint returns the midpoint' do
-          expect(subject.midpoint).to eql(12.0)
+          expect(subject.midpoint).to eql(13.0)
         end
 
       end
@@ -39,19 +39,19 @@ module Steno
         subject { end_th }
 
         it 'spans a range covering intermediate keys' do
-          expect(subject.span).to eql({start: 11, width: 11})
+          expect(subject.span).to eql({start: 12, width: 11})
         end
         it 'returns matchers labelled dark/light' do
           expect(subject.matchers).to eql([
-            {start: 11, width: 2, shade: 'light'},
-            {start: 21, width: 1, shade: 'dark'},
+            {start: 12, width: 2, shade: 'light'},
+            {start: 22, width: 1, shade: 'dark'},
           ])
         end
         it 'returns the text label' do
           expect(subject.label).to eql('th')
         end
         it '#midpoint returns the midpoint' do
-          expect(subject.midpoint).to eql(16.5)
+          expect(subject.midpoint).to eql(17.5)
         end
 
       end
@@ -60,15 +60,15 @@ module Steno
         subject { end_nch }
 
         it 'spans a range covering intermediate keys' do
-          expect(subject.span).to eql({start: 15, width: 4})
+          expect(subject.span).to eql({start: 16, width: 4})
         end
 
         it 'returns matchers labelled dark/light' do
           expect(subject.matchers).to eql([
-            {start: 15, width: 1, shade: 'dark'},
-            {start: 16, width: 1, shade: 'light'},
-            {start: 17, width: 1, shade: 'dark'},
-            {start: 18, width: 1, shade: 'light'},
+            {start: 16, width: 1, shade: 'dark'},
+            {start: 17, width: 1, shade: 'light'},
+            {start: 18, width: 1, shade: 'dark'},
+            {start: 19, width: 1, shade: 'light'},
           ])
         end
 
@@ -77,7 +77,7 @@ module Steno
         end
 
         it '#midpoint returns the point between start and end' do
-          expect(subject.midpoint).to eql(17.0)
+          expect(subject.midpoint).to eql(18.0)
         end
 
       end
@@ -92,7 +92,7 @@ module Steno
       end
 
       it '#midpoint is adjusted to accommodate the cover brick' do
-        expect(subject.midpoint).to eql(18.0)
+        expect(subject.midpoint).to eql(19.0)
       end
     end
 
