3956c5b2c282169864075f2f43089f80747b2e7c
Drew Neil <andrew.jr.neil@gmail.com>
2015-12-06 15:14:33 +0000

    Create a NotationMapper for turning notation into numeric code

--- lib/definition_exploder.rb
+++ lib/definition_exploder.rb
@@ -1,4 +1,27 @@
-class DefinitionExploder
+module NotationMapper
+  KEY_LABELS = %w(# s t k p w h r a o * e u f r p b l g t s d z)
+  #               0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2
+  #                                  10                  20
+
+  def self.translate(notation)
+    left, right = notation.downcase.split('-')
+
+    [
+      lookup(left.to_s.chars),
+      lookup(right.to_s.chars, 10)
+    ].flatten
+  end
+
+  def self.lookup(characters, offset=0)
+    Array.new.tap do |buttons|
+      KEY_LABELS[offset..-1].each_with_index do |button, index|
+        if button == characters.first
+          buttons << (index + offset)
+          characters.shift
+        end
+      end
+    end
+  end
 
 end
 
--- spec/definition_exploder_spec.rb
+++ spec/definition_exploder_spec.rb
@@ -1,47 +1,17 @@
 require 'rspec'
 require_relative '../lib/definition_exploder'
 
-describe DefinitionExploder do
-  let(:bricks) do
-    [
-      {
-        "id": "start-s",
-        "label": "S",
-        "keystrokes": [1]
-      },
-      {
-        "id": "short-a",
-        "label": "A",
-        "keystrokes": [8]
-      },
-      {
-        "id": "long-a",
-        "label": "AY",
-        "keystrokes": [8,11,12]
-      },
-      {
-        "id": "end-f",
-        "label": "F",
-        "keystrokes": [13]
-      },
-      {
-        "id": "end-v",
-        "label": "V",
-        "keystrokes": [13]
-      }
-    ]
-  end
-
-  let(:save_notation) { 'SAF' }
-  let(:safe_notation) { 'SAEUF' }
-
-  it 'works like this' do
-    exploder = DefinitionExploder.new(bricks)
-    possibilities = exploder.explode(save_notation)
-    expect(possibilities).to eql([
-      ["start-s", "short-a", "end-f"],
-      ["start-s", "short-a", "end-v"],
-    ])
+describe NotationMapper do
+  describe '#translate' do
+    it 'converts "ST" to [1, 2]' do
+      expect(NotationMapper.translate('ST')).to eql([1, 2])
+    end
+    it 'converts "-TS" to [19, 20]' do
+      expect(NotationMapper.translate('-TS')).to eql([19, 20])
+    end
+    it 'converts "STTS" to [1, 2, 19, 20]' do
+      expect(NotationMapper.translate('STTS')).to eql([1, 2, 19, 20])
+    end
   end
 end
 
